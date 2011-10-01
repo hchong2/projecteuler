27ed76f19d096f5e8cbc735f2e4a0430b816edd3
hon-kyu chong <honkyuchong@gmail.com>
2011-10-01 01:53:09 -0500

    Solved problem 12.

---
+++ Problem12.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+
+public class Problem12 {
+
+	/**
+	 * Euler Project Problem 12
+	 * http://projecteuler.net/problem=12
+	 */ 
+	public static void main(String[] args) {
+		int triangleNumber = 0;
+		for(int i=1; true; i++){
+			triangleNumber += i;
+			int answer = numberOfDivisors(triangleNumber);
+			System.out.println(triangleNumber + " " + answer + " " + i );
+			if(answer >= 500){
+				System.out.println("Answer: " + triangleNumber);
+				break;
+			}	
+		}
+	}
+	static int numberOfDivisors(int number){
+		HashSet<Integer> set = new HashSet<Integer>();
+		int limit = number;
+		for(int i=1; i<=limit; i++){
+			if(number%i == 0){
+				limit = number/i;
+				set.add(limit);
+				set.add(i);
+			}
+		}
+		return set.size();
+	}
+}
