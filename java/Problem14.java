0b763913824d792c84fa4cd4ebf83ede05080e52
hon-kyu chong <honkyuchong@gmail.com>
2011-10-04 22:18:35 -0500

    Revert "Learning how to use git.."
    This reverts commit 05f77141f809a81a44ada39572567eb2634c86db.

---
+++ java/Problem14.java
@@ -0,0 +1,43 @@
+public class Problem14 {
+	/**
+	 * Euler Project Problem 14 
+	 * http://projecteuler.net/problem=14
+	 * 
+	 * Find starting number, less than a million, with longest chain
+	 * Brute force approach.
+	 */
+	public static void main(String [] args){
+		double startTime = System.currentTimeMillis();
+		final int million = 1000000;
+		
+		double max = getChainLength(2);
+		double maxN = 0;
+		System.out.print("Calculating...");
+		for(int i=3; i<=million; i++){
+			if(i%100000 == 0)
+				System.out.print(".");
+			double num = getChainLength(i);
+			if(num>max){
+				max = num;
+				maxN = i;
+			}
+		}
+		System.out.println("\nAnswer: " + (int)maxN + " Length: " + (int)max);
+		double endTime = System.currentTimeMillis();
+		System.out.println("Time: " + (double)(endTime-startTime)/1000 +" seconds");
+	}
+	static double getChainLength(double number){
+		int count = 1;
+		while(number > 1){
+			//System.out.println(number);
+			if(number%2 == 0){
+				number = number/2;
+			}
+			else{
+				number = 3*number+1;
+			}
+			count++;
+		}
+		return count;
+	}
+}
---
+++ java/src/Problem67.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem67 {
+	/**
+	 * Euler Project Problem 67
+	 * http://projecteuler.net/problem=67
+	 */	
+	public static void main(String [] args)throws Exception{
+		List<int[]> list = new ArrayList<int[]>();
+		
+		BufferedReader br = new BufferedReader(new FileReader("../Problem67.txt"));
+		String line = br.readLine();
+		while(line!=null){
+			list.add(convertToIntegerArray(line.split(" ")));
+			line = br.readLine();
+		}
+		double startTime = System.currentTimeMillis();
+		for(int i=list.size()-2; i>=0; i--){
+			for(int y=0; y<list.get(i).length; y++){
+				int a = list.get(i+1)[y];
+				int b = list.get(i+1)[y+1];;
+				list.get(i)[y] += Math.max(a, b);
+			}
+		}
+		double endTime = System.currentTimeMillis();
+		for(int [] a: list){
+			for(int b: a){
+				System.out.print(b+ " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Answer: " + list.get(0)[0]);
+		System.out.println("Time: " + (double)(endTime-startTime)/1000 +" seconds");
+	}
+	static int[] convertToIntegerArray(String [] s){
+		int[] i = new int[s.length];
+		for(int x=0; x<s.length; x++){
+			i[x] = Integer.parseInt(s[x]);
+		}
+		return i;
+	}
+}
